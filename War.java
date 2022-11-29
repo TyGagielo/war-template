dc0bd85e7ab64b1c0497ab2266cbd59eaa75a0db
Ty Gagielo <ty.gagielo@mrjaffesclass.com>
2022-11-29 13:00:50 -0800

    only have the special condition left

--- War.java
+++ War.java
@@ -35,8 +35,7 @@ public class War
         
         System.out.println("after:");
         
-        System.out.println("Player 1's deck size - " + hands[0].getDeckSize());
-        System.out.println("Player 2's deck size - " + hands[1].getDeckSize());
+        printHandsSizes();
     }
     
     /**
@@ -49,23 +48,25 @@ public class War
         int loopNum = 1;
         
         while (hands[0].getDeckSize() != 0 && hands[1].getDeckSize() != 0 && loopNum <= 3000){
-            // adds cards to the array and appends the winner
-            
-            System.out.println("Battle #" + loopNum);
             
             if(hands[0].getCard().getRank() > hands[1].getCard().getRank()){
                 
+                System.out.println("Battle #" + loopNum);
                 System.out.println("Player 1's card, "+hands[0].getCard().getFace()+" of "+hands[0].getCard().getSuit()+", is greater than Player 2's card, "+hands[1].getCard().getFace()+" of "+hands[1].getCard().getSuit()+".");
                 
-                addCardsToCardAdder();
-               
+                addCardsToCardAdder(); 
+                
                 hands[0].addCardGroup(battlingCards);
                 
                 arrayListClear(battlingCards);
                 
+                printHandsSizes();
+                loopNum++;
+                
             } else{
                 if(hands[1].getCard().getRank() > hands[0].getCard().getRank()){
                     
+                    System.out.println("Battle #" + loopNum);
                     System.out.println("Player 2's card, "+hands[1].getCard().getFace()+" of "+hands[1].getCard().getSuit()+", is greater than Player 1's card, "+hands[0].getCard().getFace()+" of "+hands[0].getCard().getSuit()+".");
                     
                     addCardsToCardAdder();
@@ -74,30 +75,42 @@ public class War
                     
                     arrayListClear(battlingCards);
                     
+                    printHandsSizes();
+                    loopNum++;
+                    
                 } else{                
                     
-                    System.out.println("It's time to d-d-d-d d-d-d- DUUUEEELLL!!!");
+                    System.out.println("It's time to d-d-d- DUEEEL!!!");
                     
                     for (int i = 0; i < 2; i++){
                         
                         addCardsToCardAdder();
+                        System.out.println(i+1);
+                        
                     }
+                    System.out.println("3");
                 }
             }
-            
-            loopNum++;
             
             System.out.println();
         }
         
-        
+        checkForWinner();
+    }
+    
+    /**
+     * chooses a winner
+     */
+    public void checkForWinner(){
         if (hands[0].getDeckSize() == 0 || hands[0].getDeckSize() < hands[1].getDeckSize()){
             //p;ayer[0] win
             System.out.println("Player 2 WINS");
         }
-        if (hands[1].getDeckSize() == 0 || hands[1].getDeckSize() < hands[0].getDeckSize()){
+        else if (hands[1].getDeckSize() == 0 || hands[1].getDeckSize() < hands[0].getDeckSize()){
             //p;ayer[1] win
             System.out.println("Player 1 WINS");
+        } else{
+            System.out.println("Nobody wins.");
         }
     }
     
@@ -120,6 +133,14 @@ public class War
             
             full.remove(0);
         }
+    }
+    
+    /**
+     * prints the sizes of both hands
+     */
+    public void printHandsSizes(){
+        System.out.println("Player 1's deck size - " + hands[0].getDeckSize());
+        System.out.println("Player 2's deck size - " + hands[1].getDeckSize());
     }
     
     /**
