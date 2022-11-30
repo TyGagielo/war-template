558219fa232662e7a98f4ac9cbd6d2029abba20d
Ty Gagielo <ty.gagielo@mrjaffesclass.com>
2022-11-30 12:51:27 -0800

    finished war and added comments

--- War.java
+++ War.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Date;
 
 /**
  * War game class
@@ -19,6 +20,7 @@ public class War
     private Deck mainD;
     private Deck[] hands;
     
+    
     public War()
     {
         // Initializations here...
@@ -31,35 +33,45 @@ public class War
         hands = mainD.dealDeck();
         
         // ...then run the event loop
-        this.runEventLoop(1);
+        this.runEventLoop(500);
         
         System.out.println("after:");
         
         printHandsSizes();
     }
     
+    
     /**
      * This is the game's event loop. The code in here should come
      * from the War flowchart you created for this game
      */
-    public void runEventLoop(int loops) {
+    public void runEventLoop(int ms) {
         //while Loop test 1
-        
         int loopNum = 1;
         
-        while (hands[0].getDeckSize() != 0 && hands[1].getDeckSize() != 0 && loopNum <= 3000){
+        
+        while (hands[0].getDeckSize() != 0 && hands[1].getDeckSize() != 0 && loopNum <= 300){
             
             if(hands[0].getCard().getRank() > hands[1].getCard().getRank()){
                 
                 System.out.println("Battle #" + loopNum);
-                System.out.println("Player 1's card, "+hands[0].getCard().getFace()+" of "+hands[0].getCard().getSuit()+", is greater than Player 2's card, "+hands[1].getCard().getFace()+" of "+hands[1].getCard().getSuit()+".");
+                System.out.println("Player 1's card, "
+                                    +hands[0].getCard().getFace()+" of "
+                                    +hands[0].getCard().getSuit()
+                                    +", is greater than Player 2's card, "
+                                    +hands[1].getCard().getFace()+" of "
+                                    +hands[1].getCard().getSuit()+".");
                 
+                //adds top cards from both hands to battlingCards arraylist
                 addCardsToCardAdder(); 
                 
+                //adds battlingCards to winning hand
                 hands[0].addCardGroup(battlingCards);
                 
+                //clear battlingCards
                 arrayListClear(battlingCards);
                 
+                //print
                 printHandsSizes();
                 loopNum++;
                 
@@ -67,24 +79,41 @@ public class War
                 if(hands[1].getCard().getRank() > hands[0].getCard().getRank()){
                     
                     System.out.println("Battle #" + loopNum);
-                    System.out.println("Player 2's card, "+hands[1].getCard().getFace()+" of "+hands[1].getCard().getSuit()+", is greater than Player 1's card, "+hands[0].getCard().getFace()+" of "+hands[0].getCard().getSuit()+".");
+                    System.out.println("Player 2's card, "
+                                        +hands[1].getCard().getFace()+" of "
+                                        +hands[1].getCard().getSuit()
+                                        +", is greater than Player 1's card, "
+                                        +hands[0].getCard().getFace()+" of "
+                                        +hands[0].getCard().getSuit()+".");
                     
+                    //adds top cards from both hands to battlingCards arraylist
                     addCardsToCardAdder();
                     
+                    //adds battlingCards to winning hand
                     hands[1].addCardGroup(battlingCards);
                     
+                    //clear battlingCards
                     arrayListClear(battlingCards);
                     
+                    //print
                     printHandsSizes();
                     loopNum++;
                     
-                } else{                
+                } else{
                     
                     System.out.println("It's time to d-d-d- DUEEEL!!!");
                     
-                    for (int i = 0; i < 2; i++){
+                    //num of times the loop runs 
+                    int loops = 2;
+                    if (hands[0].getDeckSize() < loops || hands[1].getDeckSize() < loops){
+                        loops--;
+                    }
+                    
+                    
+                    for (int i = 0; i < loops; i++){
                         
                         addCardsToCardAdder();
+                        //print
                         System.out.println(i+1);
                         
                     }
@@ -93,10 +122,18 @@ public class War
             }
             
             System.out.println();
+            //delay
+            try{
+                Thread.sleep(ms);
+            }catch(InterruptedException ex){
+                ex.printStackTrace();
+            }
+            
         }
         
         checkForWinner();
     }
+    
     
     /**
      * chooses a winner
@@ -105,14 +142,21 @@ public class War
         if (hands[0].getDeckSize() == 0 || hands[0].getDeckSize() < hands[1].getDeckSize()){
             //p;ayer[0] win
             System.out.println("Player 2 WINS");
+            
+            hands[1].addCardGroup(battlingCards);
+            
         }
         else if (hands[1].getDeckSize() == 0 || hands[1].getDeckSize() < hands[0].getDeckSize()){
             //p;ayer[1] win
             System.out.println("Player 1 WINS");
+            
+            hands[0].addCardGroup(battlingCards);
+            
         } else{
             System.out.println("It's a TIE. Nobody wins");
         }
     }
+    
     
     /**
      * Adds cards to an arrayList and removes them
@@ -121,6 +165,7 @@ public class War
         battlingCards.add(hands[1].dealCardFromDeck());
         battlingCards.add(hands[0].dealCardFromDeck());
     }
+    
     
     /**
      * Removes all Cards in an arrayList
@@ -135,6 +180,7 @@ public class War
         }
     }
     
+    
     /**
      * prints the sizes of both hands
      */
@@ -143,11 +189,11 @@ public class War
         System.out.println("Player 2's deck size - " + hands[1].getDeckSize());
     }
     
+    
     /**
      * The main method is called when Java starts your program
      */
     public static void main(String[] args) {
         War war = new War();
     }
-
 }
